63affb876f1b1745fc6712740e07be5150b2c686
PRATHIK CHANDRA HEGDE <121326671+prathikh1998@users.noreply.github.com>
2023-06-22 02:02:34 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('query.html')
+    return app.send_static_file("index.html")
 
-@app.route('/query', methods=['POST'])
-def handle_query():
-    # Retrieve form data
-    attribute = request.form.get('attribute')
-    interval = request.form.get('interval')
-
-    # Process the query and generate chart data
-    # Replace this with your own logic to fetch data and create the chart data
-    chart_data = {
-        'labels': ['Label 1', 'Label 2', 'Label 3'],
-        'values': [10, 20, 30],
+@app.route("/get-results")
+def get_results():
+    query = request.args.get("query")
+    # Process the query and retrieve the results (dummy example)
+    results = {
+        "labels": ["Label 1", "Label 2", "Label 3"],
+        "data": [10, 20, 30]
     }
+    return jsonify(results)
 
-    return jsonify(chart_data)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
